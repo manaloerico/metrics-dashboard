6122beb5ea46ed8f36362b0df9273d89969af7f0
Echo <manaloerico.games@gmail.com>
2025-11-24 20:18:21 +0800

    update email login ui

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  hidePassword = signal(true);
   loginForm = this.fb.group({
     email: [''],
     password: [''],
@@ -33,7 +46,14 @@ export class LoginComponent {
   login() {
     this.auth.login(this.email!, this.password!).subscribe({
       next: () => this.router.navigate(['/dashboard']),
-      error: (err) => alert(err.error.message || 'Login failed'),
+      error: (err) => {
+        this.loginForm.setErrors({ invalidLogin: true });
+      },
     });
+  }
+
+  togglePasswordVisibility(event: Event): void {
+    this.hidePassword.set(!this.hidePassword());
+    event.stopPropagation();
   }
 }
